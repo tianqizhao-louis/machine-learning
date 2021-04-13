70a0c82ad55e98828ad1458c9de8c45033db99fb
Tianqi Zhao <louiszhao0517@yahoo.com>
2021-04-13 11:49:54 -0400

    Update perceptron.py

--- binary classification/perceptron.py
+++ binary classification/perceptron.py
@@ -60,7 +60,7 @@ X,y = load_iris(return_X_y=False)
 # plt.show()
 
 per = Perceptron(learning_rate=0.1, n_iter=100, random_state=1)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y) # when not continuous features
 
 per.fit(X_train, y_train)
 print(per.score(X_test, y_test), per.score(X_train, y_train))
